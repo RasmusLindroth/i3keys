8eea1f80aca5b5cb1a505c77ec058c4289af3cab
elder-n00b <elder.noob@yandex.com>
2022-10-21 20:15:44 +0200

    html,js fix

--- web/res/index.js
+++ web/res/index.js
@@ -260,8 +260,32 @@ document.getElementById('select-ansi').addEventListener('click', function () {
 document.getElementById('select-iso').click();
 */
 document.getElementById('opt-layout').addEventListener('change', (event) => {
-    let l = event.currentTarget.value;
-    var url = new URL(window.location.href);
-    url.searchParams.set('layout', l);
+    let v = event.currentTarget.value;
+    let url = new URL(window.location.href);
+    url.searchParams.set('layout', v);
     window.location.replace(url.toString());
 });
+document.getElementById('opt-captions').addEventListener('change', (event) => {
+    let v = event.currentTarget.value;
+    let kc = document.getElementsByClassName("txt");
+    switch(v) {
+        case "character":
+            for(e of kc) {
+                e.innerHTML = "..."
+            }
+            break;
+        case "symbol_split":
+            for(e of kc) {
+                e.innerHTML = e.dataset.symbol
+                    .replaceAll(/grave|acute|left|right|slash/g," $&") // TODO: smarter, more complete regex
+                    .replaceAll("_"," ")
+                    .replaceAll(" ","<br>")
+            }
+            break;
+        default:
+            for(e of kc) {
+                e.innerHTML = e.dataset.symbol
+            }
+    }
+});
+document.getElementById("opt-captions-sym").click
