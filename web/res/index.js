7dc26e0c8513dcd32ee1fd055a5da38590f9bc15
elder-n00b <elder.noob@yandex.com>
2022-10-22 12:09:35 +0200

    key-info, captions, fixes, misc
    fix key info on hover
    
    key-info, captions, fixes, misc
    
    fix key-info
    
    fix key-info
    
    nitpicking
    
    more key-info
    
    more key-info and captions
    
    more key-info
    
    default to split captions
    
    fix default
    
    restore character map

--- web/res/index.js
+++ web/res/index.js
@@ -1,6 +1,96 @@
 
-let keyboardHolder = document.querySelector('#keyboard-holder');
-let stickyHeader = document.querySelector('#sticky-header');
+let keyCaptions = {
+    "space":        "Space",
+    "exclam":       "!",
+    "quotedbl":     "\"",
+    "numbersign":   "#",
+    "dollar":       "$",
+    "percent":      "%",
+    "ampersand":    "&amp;",
+    "quoteright":   "'",
+    "parenleft":    "(",
+    "parenright":   ")",
+    "asterisk":     "*",
+    "plus":         "+",
+    "comma":        ",",
+    "minus":        "-",
+    "period":       ".",
+    "slash":        "/",
+    "colon":        ":",
+    "semicolon":    ";",
+    "less":         "<",
+    "equal":        "=",
+    "greater":      ">",
+    "question":     "?",
+    "at":           "@",
+    "bracketleft":  "[",
+    "backslash":    "\\",
+    "bracketright": "]",
+    "asciicircum":  "^",
+    "underscore":   "_",
+    "quoteleft":    "&#96;",
+    "braceleft":    "{",
+    "bar":          "|",
+    "braceright":   "}",
+    "asciitilde":   "~",
+    "agrave":       "à",
+    "aacute":       "á",
+    "acircumflex":  "â",
+    "atilde":       "ã",
+    "adiaeresis":   "ä",
+    "aring":        "å",
+    "ae":           "æ",
+    "ccedilla":     "ç",
+    "egrave":       "è",
+    "eacute":       "é",
+    "ecircumflex":  "ê",
+    "ediaeresis":   "ë",
+    "igrave":       "ì",
+    "iacute":       "í",
+    "icircumflex":  "î",
+    "idiaeresis":   "ï",
+    "eth":          "ð",
+    "ntilde":       "ñ",
+    "ograve":       "ò",
+    "oacute":       "ó",
+    "ocircumflex":  "ô",
+    "otilde":       "õ",
+    "odiaeresis":   "ö",
+    "division":     "÷",
+    "ooblique":     "ø",
+    "ugrave":       "ù",
+    "uacute":       "ú",
+    "ucircumflex":  "û",
+    "udiaeresis":   "ü",
+    "yacute":       "ý",
+    "thorn":        "þ",
+    "ydiaeresis":   "ÿ",
+    "KP_Multiply":  "KP *",
+    "KP_Add":       "KP +",
+    "KP_Subtract":  "KP -",
+    "KP_Decimal":   "KP .",
+    "KP_Divide":    "KP /",
+    "KP_Equal":     "KP ="
+}
+
+function breakSpaces(s) { return s.replaceAll(" ","<br/>") }
+
+function keyCaption(s) {
+    let c = keyCaptions[s];
+    return c ? c : splitSymbol(s);
+}
+    
+function splitSymbol(s) {
+    return s
+        .replaceAll(/grave|acute|left|right|slash/g," $&") // TODO: smarter, more complete regex
+        .replaceAll("_"," ")
+        ;
+}
+
+let stickyHeader = document.getElementById('sticky-header');
+let keyboardHolder = document.getElementById('keyboard-holder');
+let keyInfoHolder = document.getElementById('key-info');
+let keyElements = document.getElementsByClassName('key');
 
 document.getElementById('opt-layout').addEventListener('change', (event) => {
     let v = event.currentTarget.value;
@@ -12,26 +102,49 @@ document.getElementById('opt-layout').addEventListener('change', (event) => {
 document.getElementById('opt-captions').addEventListener('change', (event) => {
     let v = event.currentTarget.value;
     let kc = document.getElementsByClassName("txt");
-    switch(v) {
-        case "character":
-            for(e of kc) {
-                e.innerHTML = "..."
-            }
-            break;
-        case "symbol_split":
-            for(e of kc) {
-                e.innerHTML = e.dataset.symbol
-                    .replaceAll(/grave|acute|left|right|slash/g," $&") // TODO: smarter, more complete regex
-                    .replaceAll("_"," ")
-                    .replaceAll(" ","<br>")
-            }
-            break;
-        default:
-            for(e of kc) {
-                e.innerHTML = e.dataset.symbol
-            }
+    for(e of kc) {
+        switch(v) {
+            case "character":
+                    e.innerHTML = breakSpaces(keyCaption(e.dataset.symbol));
+                    break;
+            case "symbol":
+                    e.innerHTML = e.dataset.symbol;
+                    break;
+            case "symbol_split":
+                    e.innerHTML = breakSpaces(splitSymbol(e.dataset.symbol));
+                    break;
+            case "symbol_code":
+                    e.innerHTML = e.dataset.symbolcode;
+                    break;
+            case "identifier":
+                    e.innerHTML = e.dataset.identifier;
+                    break;
+            default:
+                    e.innerHTML = "?"; // should not happen
+        }
     }
 });
 
-document.getElementById("opt-captions-sym").selected = true;
+// bubbling event for all the .key elements
+keyboardHolder.addEventListener('mouseover', event => {
+    for (el of keyElements) {
+        if (el == event.target) {
+            if (key = event.target.firstChild) { // key is actually .txt
+                // TODO: somehow generate from range
+                document.getElementById('key-command').innerHTML=key.dataset.bindcommand;
+                document.getElementById('key-symbol').innerHTML=key.dataset.symbol;
+                document.getElementById('key-symbolcode').innerHTML=key.dataset.symbolcode;
+                document.getElementById('key-identifier').innerHTML=key.dataset.identifier;
+            } else {
+                document.getElementById('key-command').innerHTML="";
+                document.getElementById('key-symbol').innerHTML="";
+                document.getElementById('key-symbolcode').innerHTML="";
+                document.getElementById('key-identifier').innerHTML="";
+            }
+            break
+        }
+    }
+});
 
+document.getElementById("opt-captions").value = "symbol_split";
+document.getElementById("opt-captions").dispatchEvent(new Event('change'));
